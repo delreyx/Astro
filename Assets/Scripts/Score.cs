62072bdaccffd7f79b8f764722ee38b5788712ed
delreyx <ionut.tanase.a101118@mcast.edu.mt>
2022-12-05 20:34:58 +0100

    Update Score.cs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -15,7 +15,7 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.FindGameObjectWithTag("player") != null)
+        if (GameObject.FindGameObjectWithTag("Player") != null)
         {
             score += 1 * Time.deltaTime;
             ScoreText.text = ((int)score).ToString();
