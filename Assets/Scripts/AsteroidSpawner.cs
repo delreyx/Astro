ef0f99e1385d599a2e8097115d96b7609cebc3a3
delreyx <ionut.tanase.a101118@mcast.edu.mt>
2022-12-05 15:23:59 +0100

    Asteroid Spawner
    +Added Asteroids Spawner

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-    // spawner for asteroids
-    [SerializeField]
-    private float xLimit;
-    [SerializeField]
-    private float[] xPositions;
-    [SerializeField]
-    private GameObject[] enemyPrefabs;
-    [SerializeField]
-    private Wave[] wave;
+     public GameObject Asteroid;
+     public Timer timer;
+     public float radius = 3.5f; 
+     public Transform[] spawnPoint;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine("SpawnRoutine");
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (timer.duration > 0)
+        {   
+            //Find out what the point on X we can spawn.
+            //float x = Random.Range(-radius, radius);
+            int index = Random.Range(0, spawnPoint.Length);
+            Vector3 pos = spawnPoint[index].position;
+
+            Instantiate(Asteroid, pos, Quaternion.identity);
+            yield return new WaitForSeconds (1f);
+
+        }
+
     
 }
-
-[System.Seralizable]
-public class Wave
-{
-    public float delayTime;
-    public float spawnAmount;
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // The duration of a match
+    public float duration = 600;
+
+    // The visual timer representation
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        duration -= Time.deltaTime; 
+
+        if (duration <=0)
+        {
+            duration = 0;
+           
+            enabled = false;
+
+        }
+
+        
+
+    }
+}
